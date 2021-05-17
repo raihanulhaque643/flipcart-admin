62da724b659f48a4dff49e25cf71784cdd6448bb
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-17 13:59:25 +0600

    redux store setup with auth reducers and auth actions

--- src/containers/Signin/index.js
+++ src/containers/Signin/index.js
@@ -1,16 +1,31 @@
-import React from 'react'
-import Layout from '../../components/Layout'
-import Input from '../../components/UI/Input'
-import { Container, Form, Row, Col, Button } from 'react-bootstrap'
+import React from 'react';
+import Layout from '../../components/Layout';
+import Input from '../../components/UI/Input';
+import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import { login } from '../../actions';
+import { useDispatch } from 'react-redux';
 
 const Signin = () => {
+
+    const dispatch = useDispatch();
+
+    const userLogin = (e) => {
+        e.preventDefault();
+        const user = {
+            email: 'raihannn@gmail.com',
+            password: '123456'
+        }
+
+        dispatch(login(user));
+    }
+
     return (
         <div>
             <Layout>
                 <Container>
                     <Row style={{marginTop: '50px'}}>
                         <Col md={{span: 6, offset: 3}}>
-                            <Form>
+                            <Form onSubmit={userLogin}>
                                 <Input 
                                 label="Email"
                                 placeholder="Email"
