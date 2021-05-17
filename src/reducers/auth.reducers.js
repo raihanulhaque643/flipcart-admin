24dbc531b15dd5e0e45b076b9607789de84afeef
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-17 19:00:58 +0600

    admin signout api applied and store actions and reducers for signout also set

--- src/reducers/auth.reducers.js
+++ src/reducers/auth.reducers.js
@@ -1,43 +1,58 @@
 import { authConstants } from "../actions/constants";
 
 const initState = {
-    token: null,
-    user: {
-        firstName: '',
-        lastName: '',
-        email: '',
-        picture: ''
-    },
-    authenticate: false, 
-    authenticating: false,
+  token: null,
+  user: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    picture: "",
+  },
+  authenticate: false,
+  authenticating: false,
+  loading: false,
+  error: null,
+  message: ''
 };
 
 export const authReducer = (state = initState, action) => {
+  console.log(action);
 
-    console.log(action);
+  switch (action.type) {
+    case authConstants.LOGIN_REQUEST:
+      state = {
+        ...state,
+        authenticating: true,
+      };
+      break;
+    case authConstants.LOGIN_SUCCESS:
+      state = {
+        ...state,
+        user: action.payload.user,
+        token: action.payload.token,
+        authenticate: true,
+        authenticating: false,
+      };
+      break;
+    case authConstants.LOGOUT_REQUEST:
+      state = {
+        ...state,
+        loading: true
+      };
+      break;
+    case authConstants.LOGOUT_SUCCESS:
+      state = {
+        ...initState
+      };
+      break;
+    case authConstants.LOGOUT_FAILURE:
+      state = {
+        ...state,
+        error: action.payload.error,
+        loading: false
+      };
+      break;
+  }
 
-    switch(action.type){
-        case authConstants.LOGIN_REQUEST: 
-            state = {
-                ...state,
-                authenticating: true
-            }
-            break;
-        case authConstants.LOGIN_SUCCESS:
-            state = {
-                ...state,
-                user: action.payload.user,
-                token: action.payload.token,
-                authenticate: true,
-                authenticating: false
-            }
-            break;
-        case authConstants.LOGOUT_REQUEST:
-            state = {
-                ...initState
-            }
-            break;
-    }
-
-    return state;
-}
+  return state;
+};
