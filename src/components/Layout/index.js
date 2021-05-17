1c4de641930c075df3c31dd46e6d979818ca50e1
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-17 19:18:14 +0600

    sideber setup

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import Header from '../Header'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import Header from '../Header';
 
 const Layout = (props) => {
     return (
         <>
         <Header />
-        {props.children}
+        {
+            props.sidebar ? 
+            <Container fluid>
+                <Row>
+                    <Col md={2} className="sidebar">
+                    <ul>
+                        <li><NavLink to={"/"}>Home</NavLink></li>
+                        <li><NavLink to={"/products"}>Products</NavLink></li>
+                        <li><NavLink to={"/orders"}>Orders</NavLink></li>
+                    </ul>
+                    </Col>
+                    <Col md={10} style={{marginLeft: 'auto'}} >
+                        {props.children}
+                    </Col>
+                </Row>
+            </Container>
+            :
+            props.children
+        }
         </>
     )
 }
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Container, Jumbotron, Row } from "react-bootstrap";
 import Layout from "../../components/Layout";
+import { NavLink } from 'react-router-dom';
 import './style.css';
 
 const Home = () => {
@@ -9,10 +10,17 @@ const Home = () => {
       <Layout>
         <Container fluid>
           <Row>
-            <Col md={2} className="sidebar">Sidebar</Col>
+            <Col md={2} className="sidebar">
+              <ul>
+                <li><NavLink to={"/"}>Home</NavLink></li>
+                <li><NavLink to={"/products"}>Products</NavLink></li>
+                <li><NavLink to={"/orders"}>Orders</NavLink></li>
+              </ul>
+            </Col>
             <Col md={10} style={{marginLeft: 'auto'}} >Container</Col>
           </Row>
         </Container>
+
         {/* <Jumbotron style={{margin: '5rem', background: 'white'}} className="text-center">
                     <h1>Welcome to Admin Dashboard</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi doloribus, ullam ducimus voluptate magni vel, fugiat mollitia dolorem qui obcaecati quisquam est excepturi fugit laboriosam enim maiores distinctio autem inventore, commodi officiis quo sapiente corrupti. Impedit molestias commodi reprehenderit nam?</p>
