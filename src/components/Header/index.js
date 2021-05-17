9f37316f1b5228427d3191378572ef8eebd5c045
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-17 16:12:14 +0600

    sidebar setup and show signout button based on auth state

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,52 @@
-import React from 'react'
-import { Nav, Navbar, Container } from 'react-bootstrap'
-import { NavLink, Link } from 'react-router-dom'
+import React from "react";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
+  const auth = useSelector((state) => state.auth);
+  const renderLoggedInLinks = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Container>
+      <Nav>
+        <li className="nav-item">
+          <span className="nav-link">Signout</span>
+        </li>
+      </Nav>
+    );
+  };
+  const renderNonLoggedInLinks = () => {
+    return (
+      <Nav>
+        <li className="nav-item">
+          <NavLink to="/signin" className="nav-link">
+            Signin
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/signup" className="nav-link">
+            Signup
+          </NavLink>
+        </li>
+      </Nav>
+    );
+  };
+
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      style={{ zIndex: "1" }}
+    >
+      <Container fluid>
         {/* <Navbar.Brand href="#home">Admin Dashboard</Navbar.Brand> */}
-        <Link to="/" className="navbar-brand">Admin Dashboard</Link>
+        <Link to="/" className="navbar-brand">
+          Admin Dashboard
+        </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
+          <Nav className="mr-auto">
             {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -18,20 +54,13 @@ const Header = () => {
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown> */}
-            </Nav>
-            <Nav>
-            {/* <Nav.Link href="#deets">Signin</Nav.Link> */}
-            <li className="nav-item">
-                <NavLink to="/signin" className="nav-link">Signin</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink to="/signup" className="nav-link">Signup</NavLink>
-            </li>
-            </Nav>
-        </Navbar.Collapse>
-        </Container>
-        </Navbar>
-    )
-}
+          </Nav>
+            {auth.authenticate ? renderLoggedInLinks() : renderNonLoggedInLinks()}
 
-export default Header
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import { Jumbotron } from 'react-bootstrap'
-import Layout from '../../components/Layout'
+import React from "react";
+import { Col, Container, Jumbotron, Row } from "react-bootstrap";
+import Layout from "../../components/Layout";
+import './style.css';
 
 const Home = () => {
-    return (
-        <div>
-            <Layout>
-                <Jumbotron style={{margin: '5rem', background: 'white'}} className="text-center">
+  return (
+    <div>
+      <Layout>
+        <Container fluid>
+          <Row>
+            <Col md={2} className="sidebar">Sidebar</Col>
+            <Col md={10} style={{marginLeft: 'auto'}} >Container</Col>
+          </Row>
+        </Container>
+        {/* <Jumbotron style={{margin: '5rem', background: 'white'}} className="text-center">
                     <h1>Welcome to Admin Dashboard</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi doloribus, ullam ducimus voluptate magni vel, fugiat mollitia dolorem qui obcaecati quisquam est excepturi fugit laboriosam enim maiores distinctio autem inventore, commodi officiis quo sapiente corrupti. Impedit molestias commodi reprehenderit nam?</p>
-                </Jumbotron>
-            </Layout>
-        </div>
-    )
-}
+                </Jumbotron> */}
+      </Layout>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
