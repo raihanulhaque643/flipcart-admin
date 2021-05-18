0fe9655371d7283eb4cfc43d013e57571c71e167
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-18 16:48:50 +0600

    adding categories will be reflected on UI too

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import Header from '../Header';
+import './style.css';
 
 const Layout = (props) => {
     return (
@@ -13,7 +14,7 @@ const Layout = (props) => {
                 <Row>
                     <Col md={2} className="sidebar">
                     <ul>
-                        <li><NavLink to={"/"}>Home</NavLink></li>
+                        <li><NavLink exact to={"/"}>Home</NavLink></li>
                         <li><NavLink to={"/category"}>Category</NavLink></li>
                         <li><NavLink to={"/products"}>Products</NavLink></li>
                         <li><NavLink to={"/orders"}>Orders</NavLink></li>
