7ce5636c7ac2384aa9a1a3b1d0ecfc87f7ad0cae
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-19 17:12:25 +0600

    url config file updated

--- src/containers/Products/index.js
+++ src/containers/Products/index.js
@@ -4,7 +4,9 @@ import { Container, Row, Col, Table } from "react-bootstrap";
 import Input from "../../components/UI/Input";
 import Modal from "../../components/UI/Modal";
 import { useDispatch, useSelector } from "react-redux";
-import { addProduct } from "../../actions/product.actions";
+import { addProduct } from "../../actions";
+import "./style.css";
+import { generatePublicUrl } from "../../urlConfig";
 
 const Products = () => {
   const [name, setName] = useState("");
@@ -14,6 +16,8 @@ const Products = () => {
   const [categoryId, setCategoryId] = useState("");
   const [productPictures, setProductPictures] = useState([]);
   const [show, setShow] = useState(false);
+  const [productDetailModal, setProductDetailModal] = useState(false);
+  const [productDetails, setProductDetails] = useState(null);
   const category = useSelector((state) => state.category);
   const product = useSelector((state) => state.product);
   const dispatch = useDispatch();
@@ -59,14 +63,13 @@ const Products = () => {
 
   const renderProducts = () => {
     return (
-      <Table responsive="sm">
+      <Table style={{ fontSize: "12px" }} responsive="sm">
         <thead>
           <tr>
             <th>#</th>
             <th>Name</th>
             <th>Price</th>
             <th>Quantity</th>
-            <th>Description</th>
             {/* <th>Product Pictures</th> */}
             <th>Category</th>
           </tr>
@@ -75,12 +78,14 @@ const Products = () => {
           {product.products.length > 0
             ? product.products.map((product) => {
                 return (
-                  <tr key={product._id}>
+                  <tr
+                    onClick={() => showProductDetailModal(product)}
+                    key={product._id}
+                  >
                     <td>2</td>
                     <td>{product.name}</td>
                     <td>{product.price}</td>
                     <td>{product.quantity}</td>
-                    <td>{product.description}</td>
                     {/* <td>--</td> */}
                     <td>{product.category.name}</td>
                   </tr>
@@ -92,25 +97,8 @@ const Products = () => {
     );
   };
 
-  return (
-    <Layout sidebar>
-      <Container>
-        <Row>
-          <Col md={12}>
-            <div
-              className=""
-              style={{ display: "flex", justifyContent: "space-between" }}
-            >
-              <h3>Products</h3>
-              <button onClick={handleShow}>Add</button>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>{renderProducts()}</Col>
-        </Row>
-      </Container>
-
+  const renderAddProductModal = () => {
+    return (
       <Modal
         show={show}
         handleClose={handleClose}
@@ -173,6 +161,92 @@ const Products = () => {
           onChange={handleProductPictures}
         />
       </Modal>
+    );
+  };
+
+  const handleCloseProductDetailsModal = () => {
+    setProductDetailModal(false);
+  };
+
+  const showProductDetailModal = (product) => {
+    setProductDetails(product);
+    setProductDetailModal(true);
+  };
+
+  const renderProductDetailsModal = () => {
+    if (!productDetails) {
+      return null;
+    }
+
+    return (
+      <Modal
+        show={productDetailModal}
+        handleClose={handleCloseProductDetailsModal}
+        modalTitle={"Product Details"}
+        size="lg"
+      >
+        <Row>
+          <Col md={6}>
+            <label className="key">Name</label>
+            <p className="value">{productDetails.name}</p>
+          </Col>
+          <Col md={6}>
+            <label className="key">Price</label>
+            <p className="value">{productDetails.price}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <label className="key">Quantity</label>
+            <p className="value">{productDetails.quantity}</p>
+          </Col>
+          <Col md={6}>
+            <label className="key">Category</label>
+            <p className="value">{productDetails.category.name}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <label className="key">Description</label>
+            <p className="value">{productDetails.description}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <label className="key">Product Pictures</label>
+            <div style={{display: 'flex'}}>
+              {productDetails.productPictures.map(picture => {
+                return <div className="productImgContainer">
+                  <img src={generatePublicUrl(picture.img)} alt=""/>
+                </div>
+              })}
+            </div>
+          </Col>
+        </Row>
+      </Modal>
+    );
+  };
+
+  return (
+    <Layout sidebar>
+      <Container>
+        <Row>
+          <Col md={12}>
+            <div
+              className=""
+              style={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <h3>Products</h3>
+              <button onClick={handleShow}>Add</button>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>{renderProducts()}</Col>
+        </Row>
+      </Container>
+      {renderAddProductModal()}
+      {renderProductDetailsModal()}
     </Layout>
   );
 };
