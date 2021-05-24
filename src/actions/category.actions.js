bae1bfb6c8746a65d844f9da24d5388ecf9eedcb
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-24 16:12:37 +0600

    axios interceptors added

--- src/actions/category.actions.js
+++ src/actions/category.actions.js
@@ -28,19 +28,23 @@ export const addCategory = (form) => {
     dispatch({
       type: categoryConstants.ADD_NEW_CATEGORY_REQUEST,
     });
-    const res = await axios.post(`/category/create`, form);
-    if (res.status === 201) {
-      dispatch({
-        type: categoryConstants.ADD_NEW_CATEGORY_SUCCESS,
-        payload: { category: res.data.category },
-      });
-    } else {
-      dispatch({
-        type: categoryConstants.ADD_NEW_CATEGORY_FAILURE,
-        payload: res.data.error,
-      });
+    try {
+      const res = await axios.post(`/category/create`, form);
+      if (res.status === 201) {
+        dispatch({
+          type: categoryConstants.ADD_NEW_CATEGORY_SUCCESS,
+          payload: { category: res.data.category },
+        });
+      } else {
+        dispatch({
+          type: categoryConstants.ADD_NEW_CATEGORY_FAILURE,
+          payload: res.data.error,
+        });
+      }
+    } catch (error) {
+      // const {} = error.response;
+      console.log({ error });
     }
-    console.log(res);
   };
 };
 
@@ -62,9 +66,9 @@ export const deleteCategories = (ids) => {
         ids,
       },
     });
-    if(res.status == 204){
+    if (res.status == 204) {
       return true;
-    }else {
+    } else {
       return false;
     }
   };
--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,13 +1,40 @@
-import axios from 'axios';
-import { api } from '../urlConfig';
+import axios from "axios";
+import { api } from "../urlConfig";
+import store from "../store";
+import { authConstants } from "../actions/constants";
 
-const token = localStorage.getItem('token');
+const token = localStorage.getItem("token");
 
 const axiosInstance = axios.create({
-    baseURL: api,
-    headers: {
-        'Authorization': token ? `Bearer ${token}` : ''
-    },
+  baseURL: api,
+  headers: {
+    Authorization: token ? `Bearer ${token}` : "",
+  },
 });
+
+axiosInstance.interceptors.request.use((req) => {
+  const { auth } = store.getState();
+  if (auth.token) {
+    req.headers.Authorization = `Bearer ${auth.token}`;
+  }
+  return req;
+});
+
+axiosInstance.interceptors.request.use(
+  (res) => {
+    return res;
+  },
+  (error) => {
+    console.log(error.response);
+    const { status } = error.response;
+    if (status == 500 | status == 400) {
+      localStorage.clear();
+      store.dispatch({
+        type: authConstants.LOGOUT_SUCCESS,
+      });
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
